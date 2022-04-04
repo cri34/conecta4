344371201acd37bf3a1c8d94ad8f2593d93d2146
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-04-04 19:52:24 +0200

    creacion metodo mostrarTablero

--- Tauler.java
+++ Tauler.java
@@ -2,4 +2,14 @@ public class Tauler {
     private final int tamVT = 7;
     private final int tamHT = 8;
     private char[][] posFiches = new char[tamVT][tamHT];
+    public void mostrarTablero() {
+        int recH;
+        for (int recV = 0; recV < tamVT; recV++) {
+            for (recH = 0; recH < tamHT - 1; recH++) {
+                System.out.print("|" + posFiches[recV][recH] + "|");
+            }
+            System.out.println("|" + posFiches[recV][recH] + "|");
+        }
+        System.out.println("*************************************");
+    }
 }
