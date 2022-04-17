4b8af622429ee4ffce2b26c64bc43786907f71aa
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-04-17 14:51:08 +0200

    canvios en metodo mostrarTablero, se ha insertado numeros para indicar posiciones del tablero

--- Tauler.java
+++ Tauler.java
@@ -9,14 +9,25 @@ public class Tauler implements InputConnecta4 {
     char tipusFicha;
 
     public void mostrarTablero() {
-        int recH;
-        for (int recV = 0; recV < tamVT; recV++) {
-            for (recH = 0; recH < tamHT; recH++) {
-                System.out.print("|" + tablero[recV][recH] + "|");
+        int ultNum1chifra = 9;
+        String espacios="  ";
+        System.out.print( espacios );
+        for (int numH = 1; numH <= tamHT; numH++ ){
+            espacios=( numH > ultNum1chifra )?" ":"  ";
+            System.out.print( espacios + numH );
+        }
+        System.out.println();
+        for ( int recV = 0; recV < tamVT; recV++ ) {
+            for ( int recH = 0; recH < tamHT; recH++ ) {
+                if( recH==0 ){
+                    espacios= ( recV+1 > ultNum1chifra )? "" : " ";
+                    System.out.print( espacios + (recV+1) + " " );
+                }
+                System.out.print( "|" + tablero[recV][recH] + "|" );
             }
             System.out.println();
         }
-        System.out.println("*************************************");
+        System.out.println("************************************************************");
     }
 
     public void inicialitzarTablero() {
